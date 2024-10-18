9ee7f0eb76793336aeb393b2ef539231e1845eee
Divya Viralkumar Shah <Divyaviralkumar.shah@edu.sait.ca>
2024-10-18 10:26:44 -0600

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as IncredibleTodoListApp } from './app.json';
+
+AppRegistry.registerComponent(IncredibleTodoListApp, () => App);
